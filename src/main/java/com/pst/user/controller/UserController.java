1ad83f38ee35b8d9b8b361cc0f5e2475e5fb7206
Saikumar2501 <kurubasaikumar69@gmail.com>
2024-10-02 20:03:42 +0530

    user service changes

--- src/main/java/com/pst/user/controller/UserController.java
+++ src/main/java/com/pst/user/controller/UserController.java
@@ -1,46 +1,58 @@
 package com.pst.user.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pst.user.entity.UserEntity;
+import com.pst.user.service.UserService;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
 	
-	@GetMapping("/hello")
-	public String hello() {
-		System.out.println("hello calling...");
-		return "hello";
-	}
+	@Autowired
+	UserService service;
 	
 	@PostMapping
-	public void addUser() {
+	public UserEntity addUser(@RequestBody UserEntity user ) {
 		
+	 return	service.addUser(user);
 	}
-	
-	@GetMapping
-	public void viewAllUsers() {
+	 
+	@GetMapping("/all-users")
+	public List<UserEntity> viewAllUsers() {
+		
+	  return service.getAllUsers();
 		
 	}
 	
 	@DeleteMapping("{adharNumber}")
-	public void deleteUser(@PathVariable("adharNumber") long adharNumber) {
+	public String deleteUser(@PathVariable("adharNumber") long adharNumber) {
 		
+		return service.deleteUser(adharNumber);
 	}
 	
 	@PutMapping("{adharNumber}")
-	public void updateUser(@PathVariable("adharNumber") long adharNumber) {
+	public UserEntity updateUser(@PathVariable("adharNumber") long adharNumber,@RequestBody UserEntity user) {
 		
+		user.setAadharNumber(adharNumber);
+		return service.addUser(user);
 	}
 	
 	@GetMapping("{adharNumber}")
-	public void getUserByAdharNumber(@PathVariable("adharNumber") long adharNumber) {
+	public Optional<UserEntity> getUserByAdharNumber(@PathVariable("adharNumber") long adharNumber) {
 		
+		return service.getUserByAadharNumber(adharNumber);
 	}
 	
 	
---
+++ src/main/java/com/pst/user/entity/UserEntity.java
@@ -0,0 +1,258 @@
+package com.pst.user.entity;
+
+import java.sql.Blob;
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class UserEntity {
+	@Id
+	private long aadharNumber;
+	
+	private String userFirstName;
+	
+	private String userLastName;
+	
+	private String gender;
+	
+	private Date dateOfBirth;
+	
+	private long mobileNumber;
+	
+	private String email;
+	
+	private String addressLine1;
+	
+	private String village;
+	
+	private String mandal;
+	
+	private String district;
+	
+	private int pin;
+	
+	private String state;
+	
+	private String country;
+	
+	private int otp;
+	
+	private String password;
+	
+	private Blob photo;
+	
+	private String createdBy;
+	
+	private String updatedBy;
+	
+	private Date createdAt;
+	
+	private Date updatedAt;
+
+	public long getAadharNumber() {
+		return aadharNumber;
+	}
+
+	public void setAadharNumber(long aadharNumber) {
+		this.aadharNumber = aadharNumber;
+	}
+
+	public String getUserFirstName() {
+		return userFirstName;
+	}
+
+	public void setUserFirstName(String userFirstName) {
+		this.userFirstName = userFirstName;
+	}
+
+	public String getUserLastName() {
+		return userLastName;
+	}
+
+	public void setUserLastName(String userLastName) {
+		this.userLastName = userLastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public long getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getVillage() {
+		return village;
+	}
+
+	public void setVillage(String village) {
+		this.village = village;
+	}
+
+	public String getMandal() {
+		return mandal;
+	}
+
+	public void setMandal(String mandal) {
+		this.mandal = mandal;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public int getPin() {
+		return pin;
+	}
+
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public int getOtp() {
+		return otp;
+	}
+
+	public void setOtp(int otp) {
+		this.otp = otp;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Blob getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(Blob photo) {
+		this.photo = photo;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public UserEntity() {
+		//empty
+	}
+
+	public UserEntity(long aadharNumber, String userFirstName, String userLastName, String gender, Date dateOfBirth,
+			long mobileNumber, String email, String addressLine1, String village, String mandal, String district,
+			int pin, String state, String country, int otp, String password, Blob photo, String createdBy,
+			String updatedBy, Date createdAt, Date updatedAt) {
+		this.aadharNumber = aadharNumber;
+		this.userFirstName = userFirstName;
+		this.userLastName = userLastName;
+		this.gender = gender;
+		this.dateOfBirth = dateOfBirth;
+		this.mobileNumber = mobileNumber;
+		this.email = email;
+		this.addressLine1 = addressLine1;
+		this.village = village;
+		this.mandal = mandal;
+		this.district = district;
+		this.pin = pin;
+		this.state = state;
+		this.country = country;
+		this.otp = otp;
+		this.password = password;
+		this.photo = photo;
+		this.createdBy = createdBy;
+		this.updatedBy = updatedBy;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+	}
+	
+	
+}
